9138acf066622250f7cc56d4142d5082db8d5f36
4elaksh <bakhur2.18@gmail.com>
2020-02-10 17:14:18 +0300

    add new line

--- src/HelloWow.java
+++ src/HelloWow.java
@@ -1,7 +1,7 @@
 public class HelloWow {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Smth New");
+        System.out.println("Smth New 1987");
 
     }
 }
